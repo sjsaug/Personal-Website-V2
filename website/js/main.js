38a2d0208cbd7d367bf257a14592049e2eff43fe
August <sjsaug@gmail.com>
2024-09-08 13:19:15 -0400

    rework (simple html site)

---
+++ website/js/main.js
@@ -0,0 +1,12 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        const href = this.getAttribute('href');
+        if (href.startsWith('#')) {
+            e.preventDefault();
+            document.querySelector(href).scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
